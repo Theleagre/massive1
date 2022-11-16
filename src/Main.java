9adcf618a16055beec174e472fcec8eb3529e155
theleagre <grld@mail.ru>
2022-11-16 21:50:43 +1000

    коммит 1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("урок");
+        int[] weights = new int[]{90, 91, 93 , 92, 85, 87, 88, 89, 0, 0, 0, 0};
+        weights[0] = 90;
+        int januaryWeight = weights[0];
+        System.out.println(januaryWeight);
+        System.out.println(weights[0]);
+        System.out.println(weights[4]);
+        int january = 0;
+        System.out.println(weights[january]);
+        for (int i = 0; i < weights.length; i++) {
+            System.out.println(weights[i]);
+
+        }
+    }
+
+
+}
